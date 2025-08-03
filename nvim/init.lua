1eeb7eaf192d15ab88cc89b70c496883975599af
Dmitry Kopievsky <kopievskyd@gmail.com>
2025-08-03 18:12:07 +0300

    nvim: add file explorer toggle function
    - add `lua/explorer.lua` with `toggle()` function to open or close netrw
    - update `init.lua` to map `<leader>e` to toggle explorer

--- nvim/init.lua
+++ nvim/init.lua
@@ -48,6 +48,7 @@ map("n", "n", "nzzzv", { desc = "Next search result (centered)" })
 map("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
 map("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+map("n", "<leader>e", require("explorer").toggle, { desc = "Toggle explore" })
 
 local autocmd = vim.api.nvim_create_autocmd
 
